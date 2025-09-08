5b6ee4bdeb7a37fb4ce9de428a593145a9ce55ee
yarkin <yarkineren@alumni.sabanciuniv.edu>
2025-09-08 11:53:59 +0200

    added clean up back to scrinshot

--- flask/routes/scrinshot.py
+++ flask/routes/scrinshot.py
@@ -297,6 +297,25 @@ def scrinshot():
         else:
             print('file_regions not found, skipped:', temp_path)
 
+    # Clean up temporary files
+    if os.path.exists(form_data['file_regions']['value']):
+        print('deleted')
+        os.remove(form_data['file_regions']['value'])
+    a = split_on_newline(form_data['files_fasta_target_probe_database']['value'])
+    if '\n' in a:
+        a.remove('\n')
+    for i in a:
+        print('deleted')
+        if os.path.exists(i):
+            os.remove(i)
+    a = split_on_newline(form_data['files_fasta_reference_database_target_probe']['value'])
+    if '\n' in a:
+        a.remove('\n')
+    for i in a:
+        print('deleted')
+        if os.path.exists(i):
+            os.remove(i)
+
     # Update run status in database
     mongo.db.runs.update_one(
         {"_id": run_id},
