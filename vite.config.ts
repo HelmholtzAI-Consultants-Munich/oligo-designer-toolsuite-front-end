935b12ec92be6b9d2b8ae808a705dc645dd495e5
maxtidev <max.timmermann@student.hpi.uni-potsdam.de>
2025-11-08 23:34:52 +0100

    build: configure vitest and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,5 +9,11 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  // https://vitest.dev/config/
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
   }
 })
