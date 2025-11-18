0c2d56dbbbb45fea750606ae8bb798783416c92a
felixdille <Felix.Dille@student.hpi.uni-potsdam.de>
2025-11-18 14:33:33 +0100

    test: mark tests that are known failures

--- flask/tests/test_pipeline_routes.py
+++ flask/tests/test_pipeline_routes.py
@@ -137,6 +137,7 @@ def test_get_files_valid_runid_unused(client, run_id):
         response = client.get(f"/api/runs/{run_id}/files")
         assert response.status_code == 404
 
+@pytest.mark.knownfailure
 def test_get_files_invalid_runid(client):
         run_id = "hallo"
 
