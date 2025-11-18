ba060715370f795cba32469fecab0f19893c7dae
loumoe <57172796+loumoe@users.noreply.github.com>
2025-11-18 16:11:21 +0100

    refactor: create fileSelection and fastaGeneration component for merfish pipeline, create helpers.ts for filehandling, create types.ts

---
+++ src/components/fileSelection.tsx
@@ -0,0 +1,64 @@
+import { OverlayTrigger, Popover } from "react-bootstrap";
+import { InfoCircle } from "react-bootstrap-icons";
+import { FileState, formData, formDataKey } from "./types";
+import { handleFileChange } from "./helpers";
+
+type Props = {
+  formData: formData;
+  id: formDataKey;
+  setFiles: React.Dispatch<React.SetStateAction<FileState>>;
+  files: any;
+};
+const FileSelection: React.FC<Props> = ({ formData, id, setFiles, files }) => {
+  let multiple = id === "files_region" ? true : false;
+
+  return (
+    <>
+      <div className="w-50 d-flex align-items-center">
+        <input
+          type="file"
+          className="form-control visually-hidden"
+          id={id}
+          name={id}
+          onChange={(e) => handleFileChange(e, setFiles)}
+          multiple={multiple}
+        />
+        <label
+          htmlFor={id}
+          className="btn btn-outline-primary me-2 w-100"
+          style={{ cursor: "pointer" }}
+        >
+          Choose File
+        </label>
+        <OverlayTrigger
+          trigger="hover"
+          placement="top"
+          overlay={
+            <Popover id={id}>
+              <Popover.Body>{formData[id].comment}</Popover.Body>
+            </Popover>
+          }
+        >
+          <InfoCircle
+            style={{
+              fontSize: "1.2rem",
+              cursor: "pointer",
+              color: "#0d6efd",
+              marginLeft: "10px",
+            }}
+          />
+        </OverlayTrigger>
+      </div>
+      <div className="text-muted small mt-1">
+        {id === "file_regions"
+          ? files[id]
+            ? `Selected: ${files[id].name}`
+            : "No files selected"
+          : files[id].length > 0
+          ? `Selected: ${files[id].map((f: File) => f.name).join(", ")}`
+          : "No files selected"}
+      </div>
+    </>
+  );
+};
+export default FileSelection;
