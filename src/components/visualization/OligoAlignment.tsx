723ae0aaccfc748abb58962e565b2381f2997dda
Simon Ament <simon.ament@student.hpi.uni-potsdam.de>
2025-12-03 15:40:32 +0100

    fix: run formatter, specify correct d3 types, move Oligo interface definition

--- src/components/visualization/OligoAlignment.tsx
+++ src/components/visualization/OligoAlignment.tsx
@@ -1,113 +1,142 @@
-import * as d3 from "d3"
+import * as d3 from "d3";
 import { useEffect } from "react";
-import type { Oligo } from "../../pages/rundetail";
-import ComponentDefinition from './oligoComponents.json';
+import ComponentDefinition from "./oligoComponents.json";
 import { reverseComplement } from "./helpers";
+import type { Oligo } from "../../types";
 
-type Props ={
+type Props = {
     oligos: Oligo[];
     pipeline: string;
     oligoIndex: number;
 };
 
-type OligoComponentDefinition = {
-    type: "entry",
-    field: string,
-    isReverseComplement?: boolean,
-    isBinding?: boolean,
-    color: string,
-    label: string,
-} | {
-    type: "sequence",
-    value: string,
-    color: string,
-    label: string,
-}
+type OligoComponentDefinition =
+    | {
+          type: "entry";
+          field: string;
+          isReverseComplement?: boolean;
+          isBinding?: boolean;
+          color: string;
+          label: string;
+      }
+    | {
+          type: "sequence";
+          value: string;
+          color: string;
+          label: string;
+      };
 
 type OligoComponent = {
-    sequence: string,
-    color: string,
-    label: string,
-    isBinding: boolean,
-}
+    sequence: string;
+    color: string;
+    label: string;
+    isBinding: boolean;
+};
 
 type OligoBase = {
-    char: string, // single character
-    color: string,
-    label: string,
-    isBinding: boolean,
-}
+    char: string; // single character
+    color: string;
+    label: string;
+    isBinding: boolean;
+};
 
-const OligoAlignment: React.FC<Props> = ({
-    oligos,
-    pipeline,
-    oligoIndex
-}) => {
+const OligoAlignment: React.FC<Props> = ({ oligos, pipeline, oligoIndex }) => {
     const components: OligoComponent[] = [];
 
-    useEffect(() => {
-        const svg = d3.select("#oligo")
-        const group = svg.select("#oligo g")
-        const zoomed = (e: any) => {
-            const transform = e.transform
-            const svgHeight = (svg.node() as SVGElement).getBoundingClientRect().height
-            transform.y = svgHeight / 2
-            group.attr('transform', transform)
-        }
-        const zoom = d3.zoom().on('zoom', zoomed)
-        //@ts-expect-error -- d3 type definitions seem incorrect
-        svg.call(zoom)
-        const svgBox = (svg.node() as SVGElement).getBoundingClientRect()
-        group.attr("transform", `translate(0, ${svgBox.height / 2})`)
-    }, [oligos])
+    const componentsToBases = (components: OligoComponent[]): OligoBase[] => {
+        return components
+            .map((component) =>
+                [...component.sequence].map((char) => {
+                    return {
+                        char,
+                        color: component.color,
+                        label: component.label,
+                        isBinding: component.isBinding,
+                    };
+                })
+            )
+            .flat();
+    };
 
-    const definition = (ComponentDefinition[(pipeline as keyof typeof ComponentDefinition)] as OligoComponentDefinition[]);
+    const definition = ComponentDefinition[
+        pipeline as keyof typeof ComponentDefinition
+    ] as OligoComponentDefinition[];
+
+    if (definition) {
+        definition.forEach((componentDef) => {
+            if (componentDef.type === "entry") {
+                let sequence = oligos[oligoIndex][
+                    componentDef.field as keyof Oligo
+                ][0][0] as string;
+                if (componentDef.isReverseComplement) {
+                    sequence = reverseComplement(sequence);
+                }
+                components.push({
+                    sequence: sequence,
+                    color: componentDef.color,
+                    label: componentDef.label,
+                    isBinding: componentDef.isBinding ?? false,
+                });
+            } else if (componentDef.type === "sequence") {
+                components.push({
+                    sequence: componentDef.value,
+                    color: componentDef.color,
+                    label: componentDef.label,
+                    isBinding: false,
+                });
+            }
+        });
+    }
+
+    useEffect(() => {
+        const width = 12 * (componentsToBases(components).length + 2);
+        const height = 100;
+        const margin = 20;
+
+        const svg = d3.select("#oligo") as d3.Selection<
+            Element,
+            unknown,
+            Element,
+            unknown
+        >;
+        const group = svg.select("#oligo g");
+        const zoomed = ({ transform }: { transform: d3.ZoomTransform }) => {
+            group.attr("transform", transform.toString());
+        };
+        const zoom = d3
+            .zoom()
+            .scaleExtent([1, 5])
+            .translateExtent([
+                [-margin, 30],
+                [width + margin, 70],
+            ])
+            .on("zoom", zoomed);
+        svg.attr("viewBox", [0, 0, width, height])
+            .attr("width", width)
+            .attr("height", height)
+            .attr("style", "width: 100%; height: auto;")
+            .call(zoom);
+        const svgBox = (svg.node() as SVGElement).getBoundingClientRect();
+        group.attr("y", `${svgBox.height / 2}`);
+    }, [components]);
 
     if (!definition) {
-        return <div>No visualization available for pipeline {pipeline}</div>
+        return <div>No visualization available for pipeline {pipeline}</div>;
     }
 
-    definition.forEach(componentDef => {
-        if (componentDef.type === "entry") {
-            let sequence = oligos[oligoIndex][componentDef.field as keyof Oligo][0][0] as string;
-            if (componentDef.isReverseComplement) {
-                sequence = reverseComplement(sequence);
-            }
-            components.push({
-                sequence: sequence,
-                color: componentDef.color,
-                label: componentDef.label,
-                isBinding: componentDef.isBinding ?? false,
-            })
-        } else if (componentDef.type === "sequence") {
-            components.push({
-                sequence: componentDef.value,
-                color: componentDef.color,
-                label: componentDef.label,
-                isBinding: false,
-            })
-        }
-    })
-
-    const componentsToBases = (components: OligoComponent[]): OligoBase[] => {
-        return components.map(component =>
-            [...component.sequence].map(char => {
-                return {
-                    char,
-                    color: component.color,
-                    label: component.label,
-                    isBinding: component.isBinding
-                }
-            })
-        ).flat()
-    }
-    return(
+    return (
         <>
-            <svg id= "oligo" width="100%">
+            <svg id="oligo">
                 <g>
-                    {componentsToBases(components).map(
-                        (base, index) => <text x={(index*12)} y={base.isBinding ? 10 : 0} style={{fill: base.color, textAnchor: "middle"}}>{base.char}</text>
-                    )}
+                    {componentsToBases(components).map((base, index) => (
+                        <text
+                            x={(index + 1) * 12}
+                            y={base.isBinding ? 55 : 45}
+                            style={{ fill: base.color, textAnchor: "middle" }}
+                        >
+                            {base.char}
+                        </text>
+                    ))}
                 </g>
             </svg>
 
@@ -116,15 +145,31 @@ const OligoAlignment: React.FC<Props> = ({
                     <div className="col col-auto">
                         <strong>Legend:</strong>
                     </div>
-                    {Array.from(new Set(componentsToBases(components).map(base => base.label))).map((label, index) => {
-                        const base = componentsToBases(components).find(base => base.label === label);
+                    {Array.from(
+                        new Set(
+                            componentsToBases(components).map(
+                                (base) => base.label
+                            )
+                        )
+                    ).map((label, index) => {
+                        const base = componentsToBases(components).find(
+                            (base) => base.label === label
+                        );
                         if (!base) return null;
                         return (
                             <div className="col col-auto" key={index}>
-                                <span style={{display: "inline-block", width: "12px", height: "12px", backgroundColor: base.color, marginRight: "5px"}}></span>
+                                <span
+                                    style={{
+                                        display: "inline-block",
+                                        width: "12px",
+                                        height: "12px",
+                                        backgroundColor: base.color,
+                                        marginRight: "5px",
+                                    }}
+                                ></span>
                                 {label}
                             </div>
-                        )
+                        );
                     })}
                 </div>
             </div>
--- src/components/visualization/helpers.ts
+++ src/components/visualization/helpers.ts
@@ -1,11 +1,20 @@
 export const reverseComplement = (sequence: string): string => {
-    return sequence.split('').reverse().map(base => {
-        switch (base) {
-            case 'A': return 'T';
-            case 'T': return 'A';
-            case 'C': return 'G';
-            case 'G': return 'C';
-            default: return base;
-        }
-    }).join('');
-}
+    return sequence
+        .split("")
+        .reverse()
+        .map((base) => {
+            switch (base) {
+                case "A":
+                    return "T";
+                case "T":
+                    return "A";
+                case "C":
+                    return "G";
+                case "G":
+                    return "C";
+                default:
+                    return base;
+            }
+        })
+        .join("");
+};
