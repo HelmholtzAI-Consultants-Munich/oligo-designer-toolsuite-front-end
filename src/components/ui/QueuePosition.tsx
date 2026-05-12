0322cca3f37af429ed62fbb17b860c33c6b9c540
Simon Ament <simon.ament@student.hpi.uni-potsdam.de>
2026-05-12 21:00:50 +0200

    feat: animate paused run status

--- src/components/ui/QueuePosition.tsx
+++ src/components/ui/QueuePosition.tsx
@@ -8,11 +8,11 @@ export default function QueuePosition({ run }: { run: PipelineRun }) {
     const [highPriorityAhead, lowPriorityAhead] = run.queue_position;
     const runsAhead = highPriorityAhead + lowPriorityAhead;
     return (
-        <Alert>
+        <Alert style={{ width: "30rem", maxWidth: "100%" }}>
             <Alert.Heading className="text-center fs-3 mb-4 mt-2">
                 Run pending...
             </Alert.Heading>
-            <p>
+            <p className="text-center">
                 {runsAhead === 0
                     ? "Your run is next in line to be processed!"
                     : `There ${runsAhead === 1 ? "is" : "are"} ${runsAhead} run${runsAhead !== 1 ? "s" : ""} ahead of you.`}
@@ -20,9 +20,10 @@ export default function QueuePosition({ run }: { run: PipelineRun }) {
             {run.priority === "default" && (
                 <>
                     <hr />
-                    <p>
-                        Note: Logged in users have priority. Consider logging in
-                        to move up the queue!
+                    <p className="small text-muted mb-0">
+                        Note: Logged in users have priority and may skip ahead
+                        of you in the queue. Consider logging in to have your
+                        runs processed faster.
                     </p>
                 </>
             )}
