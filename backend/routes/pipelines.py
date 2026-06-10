36b1bcc3c552e22fe8ddad4b915e6354684329fa
felixdille <Felix.Dille@student.hpi.uni-potsdam.de>
2026-06-10 17:22:18 +0200

    feat: use task ids as run ids

--- backend/routes/pipelines.py
+++ backend/routes/pipelines.py
@@ -125,7 +125,6 @@ def write_run_to_DB(
     pipeline_name: str,
     run_id: ObjectId,
     context: RunContext,
-    task_id: str | None,
     priority: int = CeleryConfig.task_default_priority,
     queue_position: tuple[int, int] = (0, 0),  # (high priority runs ahead, low priority runs ahead)
     pipeline_run_config: dict | None = None,
@@ -139,7 +138,6 @@ def write_run_to_DB(
         "timestamp": context.timestamp,
         "output_path": serialize_path(context.output_path),
         "pipeline": pipeline_name,
-        "task_id": task_id,
         "priority": "high" if priority == CeleryConfig.task_high_priority else "default",
         "queue_position": queue_position,
     }
@@ -199,12 +197,12 @@ def enqueue_pipeline(
     # for worker processes that do not shut down after the soft timeout.
     pipeline_signature = celery_app.signature(
         Tasks.RUN_PIPELINE,
+        task_id=str(run_id),
         args=(pipeline_name, form_data, str(context.output_path)),
         priority=priority,
         soft_time_limit=soft_limit,
         time_limit=hard_limit,
         headers={
-            "run_id": str(run_id),
             "pipeline": pipeline_name,
             "user_id": context.user_id,
             "session_id": context.session_id,
@@ -212,9 +210,9 @@ def enqueue_pipeline(
         },
     )
 
-    error_handler = celery_app.signature(Callbacks.PIPELINE_CHORD_ERRBACK, kwargs={"run_id_str": str(run_id)})
+    error_handler = celery_app.signature(Callbacks.PIPELINE_CHORD_ERRBACK)
 
-    return chord(region_generation_signatures)(pipeline_signature.on_error(error_handler))
+    chord(region_generation_signatures)(pipeline_signature.on_error(error_handler))
 
 
 def calculate_queue_position(priority: int) -> tuple[int, int]:
@@ -394,7 +392,7 @@ def start_pipeline(pipeline_name: str):
     priority = get_task_priority(form_data)
     enqueued_at = utc_now()
 
-    result_promise = enqueue_pipeline(
+    enqueue_pipeline(
         run_id,
         pipeline_name,
         form_data,
@@ -415,7 +413,6 @@ def start_pipeline(pipeline_name: str):
         pipeline_name,
         run_id,
         context,
-        result_promise.id,
         priority,
         (high_priority_ahead, default_priority_ahead),
         pipeline_run_config,
